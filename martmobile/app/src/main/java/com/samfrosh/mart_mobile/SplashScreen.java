99ef671c0fb67f3e1726e1986243912f0ce40d4c
samuel <samfrosh673@gmail.com>
2023-05-26 18:59:07 +0100

    created the splash screen page for mart mobile application

--- martmobile/app/src/main/java/com/samfrosh/mart_mobile/SplashScreen.java
+++ martmobile/app/src/main/java/com/samfrosh/mart_mobile/SplashScreen.java
@@ -3,14 +3,28 @@ package com.samfrosh.mart_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
 
+    Button shopbtn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        shopbtn = findViewById(R.id.shopbtn);
+
+        shopbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserLogin.class));
+            }
+        });
     }
 }
