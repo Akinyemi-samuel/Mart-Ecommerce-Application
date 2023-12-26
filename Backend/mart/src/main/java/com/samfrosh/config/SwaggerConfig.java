4f0f8955b595b7aeda93de50e0a096f2cad821b6
samuel <samfrosh673@gmail.com>
2023-12-26 18:43:50 +0100

    swagger config server update

---
+++ Backend/mart/src/main/java/com/samfrosh/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.samfrosh.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Akinyemi samuel",
+                        email = "Akinyemisamuelayo@gmail.com",
+                        url = "http://"
+                ),
+                description = "Mart Ecommerce Application for shopping",
+                title = "Mart Ecommerce Application",
+                version = "1.0",
+                license = @License(
+                        name = "Samuel licenses",
+                        url = "http://"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "",
+                        url = "http://localhost:8080/api/v2"
+                ),
+                @Server(
+                        description = "",
+                        url = ""
+                )
+        }
+)
+public class SwaggerConfig {
+}
