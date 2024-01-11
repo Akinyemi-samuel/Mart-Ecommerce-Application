e9acd6e5bd77c54d72e11fc104026ca8d6f5d917
samuel <samfrosh673@gmail.com>
2024-01-11 01:40:50 +0100

    updated production environment details

--- mart-stores/src/environments/environment.development.ts
+++ mart-stores/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v2',
-  loginUrl: 'http://localhost:8080/api/v2/user/login',
-  logoutUrl: 'http://localhost:8080/api/v2/user/logout',
+  apiUrl: 'https://mart.cleverapps.io/api/v2',
+    loginUrl: 'https://mart.cleverapps.io/api/v2/user/login',
+    logoutUrl: 'https://mart.cleverapps.io/api/v2/user/logout',
 };
